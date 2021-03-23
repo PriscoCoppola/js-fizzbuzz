c4efc2005e43a7bf8318802623aa56c521f7c0b0
PriscoCoppola <priscocoppola7@gmail.com>
2021-03-23 15:19:03 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@
     Per i multipli di 5 stampare "Buzz" al posto del numero
     Per i numeri che sono sia multipli di 3 che di 5 stampare FizzBuzz
 */
+
+for (var n = 1; n <= 100; n++) {
+
+    if (n % 3 === 0 && n % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (n % 5 === 0) {
+        console.log('Buzz')
+    } else if (n % 3 === 0) {
+        console.log('Fizz');
+    } else {
+        console.log(n);
+    }
+}
