7c4631ad58185bcb7a6ddbc36a92651e0eadeace
PriscoCoppola <priscocoppola7@gmail.com>
2021-03-23 14:31:19 +0100

    initialize

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*  FizzBuzz
+    Scrivi un programma che stampi i numeri da 1 a 100
+    Per i multipli di 3 stampare “Fizz” al posto del numero
+    Per i multipli di 5 stampare "Buzz" al posto del numero
+    Per i numeri che sono sia multipli di 3 che di 5 stampare FizzBuzz
+*/
